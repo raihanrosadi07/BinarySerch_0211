db4c3c0b9427a8a0abec09ec68f072ef0f864b39
raihanrosadi07 <raihanrosadi284@gmail.com>
2026-04-14 11:17:02 +0700

    void menginput

--- binaryserch.cpp
+++ binaryserch.cpp
@@ -5,3 +5,21 @@ int element[10];
 int nPanjang;
 int x;
 
+void input()
+{
+    while(true)
+    {
+        cout << "Masukkan banyak elemen pada array: ";
+        cin >> nPanjang;
+
+        if (nPanjang <= 10)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\n[!] Jumlah element tidak boleh lebih dari 10. Silahkan coba lagi.\n";
+        }
+    }
+
+    
